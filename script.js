0739bc5d38742fd8b1a1b0c607bd8e1ba7aaae9a
EmmaKespo <kespoemma@gmail.com>
2026-07-06 14:26:05 +0100

    initialize for deployment

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
-let menuIcon = document.querySelector("#menu-icon");
-let navbar = document.querySelector(".navbar");
+let menuToggle = document.querySelector("#menu-toggle"); 
+let menuIcon = document.querySelector("#menu-icon"); 
+let navbar = document.querySelector(".navbar"); 
 
-menuIcon.onclick = () => {
-    menuIcon.classList.toggle("bx-x")  //classname from boxer icon
-    navbar.classList.toggle("active")
-}
+// Triggers execution loop upon active user event click actions
+menuToggle.onclick = () => { 
+    // Toggles class target references to alter CSS layout blocks
+    navbar.classList.toggle("active"); 
+    
+    // Evaluates strings dynamically to swap presentation icons
+    if (menuIcon.classList.contains('fa-bars')) { 
+        menuIcon.classList.replace('fa-bars', 'fa-circle-xmark'); 
+    } else { 
+        menuIcon.classList.replace('fa-circle-xmark', 'fa-bars'); 
+    } 
+};
