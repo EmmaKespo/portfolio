2a721f61e5598636e144ad65d0331cefbcda3995
EmmaKespo <kespoemma@gmail.com>
2026-07-06 16:57:33 +0100

    Production: Connect form to live Render backend

--- email.js
+++ email.js
@@ -13,7 +13,7 @@
 
         try {
             // 2. Send data to your local Node.js server
-            const response = await fetch('http://localhost:3000/api/contact', {
+            const response = await fetch('https://portfolio-htlz.onrender.com/api/contact', {
                 method: 'POST',
                 headers: { 'Content-Type': 'application/json' },
                 body: JSON.stringify(formData)
